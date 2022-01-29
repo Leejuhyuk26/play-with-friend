a8729d1ca8f2e0f9648fb62558b1afca51503313
Leejuhyuk26 <gbdldl@naver.com>
2022-01-29 15:41:29 +0900

    20220129 JPA파트 진행중4 커밋

--- src/main/java/friend/with/play/springboot/test.java
+++ src/main/java/friend/with/play/springboot/test.java
@@ -61,6 +61,7 @@ public class test {
     @Test
     public void 정답() {
         Assert.assertEquals(7, solution("aabbaccc"));
+        Assert.assertEquals(7, solution("aabbaccc"));
         Assert.assertEquals(17, solution("xababcdcdababcdcd"));
     }
 }
